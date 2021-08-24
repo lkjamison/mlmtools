8453ade54a1ede5a1c6de5a41e1ce672eb73e73b
lj5yn <60159688+lj5yn@users.noreply.github.com>
2021-08-24 09:46:29 -0400

    Update caterpillarPlot.R

--- R/caterpillarPlot.R
+++ R/caterpillarPlot.R
@@ -21,103 +21,103 @@
 #' classid_caterpillarPlot <- caterpillarPlot(mod0, title = "title", grpvar = "classid", grey = TRUE)
 #'
 #' @references Rabe-Hesketh S, Skrondal A (2012). Multilevel and Longitudinal Modeling Using Stata, Vol- umes I and II, Third Edition. 3 edition edition. Stata Press. ISBN 978-1-59718-108-2.
-#' 
-#' @export caterpillarPlot
+
 
 caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, grey = FALSE){
   condsd <- condval <- grp <- interval_95 <- NULL
-
+  
   model.re <- data.frame(lme4::ranef(model))
   model.re <- model.re[which(model.re$grpvar == grpvar),]
-
+  
   model.re$int_pos2 <- model.re$condval + 2*model.re$condsd
   model.re$int_neg2 <- model.re$condval - 2*model.re$condsd
   model.re$interval_95 <- rep(NA, length(model.re$int_pos2))
-
+  
   for(i in 1:length(model.re$int_pos2)){
-
+    
     if(model.re$int_pos2[i] > 0 & model.re$int_neg2[i] > 0){
-
+      
       model.re$interval_95[i] <- "all positive"
-
+      
     }else if(model.re$int_pos2[i] < 0 & model.re$int_neg2[i] < 0){
-
+      
       model.re$interval_95[i] <- "all negative"
-
+      
     }else{
-
+      
       model.re$interval_95[i] <- "includes 0"
-
+      
     }
-
+    
   }
-
-  if(tall == TRUE){
-
+  
+  caterpillar.plot <- if(tall == TRUE){
+    
     if(grey == TRUE){
-
+      
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Grey27", "Black", "Grey87"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
         ggplot2::ylab("Group ID") +
         ggplot2::ggtitle(title) +
         ggplot2::theme(axis.text.y = ggplot2::element_blank(),
-                      axis.ticks = ggplot2::element_blank()) +
+                       axis.ticks = ggplot2::element_blank()) +
         ggplot2::geom_point() + ggplot2::facet_wrap(~term,scales="free_x") +
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
-                                   xmax=condval +2*condsd), height=0)
-
+                                             xmax=condval +2*condsd), height=0)
+      
     }else if(grey == FALSE){
-
+      
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Red", "Green", "Blue"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
         ggplot2::ylab("Group ID") +
         ggplot2::ggtitle(title) +
         ggplot2::theme(axis.text.y = ggplot2::element_blank(),
-                        axis.ticks = ggplot2::element_blank()) +
+                       axis.ticks = ggplot2::element_blank()) +
         ggplot2::geom_point() + ggplot2::facet_wrap(~term,scales="free_x") +
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
-                                     xmax=condval +2*condsd), height=0)
-
+                                             xmax=condval +2*condsd), height=0)
+      
     }
-
+    
   }else if(tall == FALSE){
-
+    
     if(grey == TRUE){
-
+      
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Grey27", "Black", "Grey87"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
         ggplot2::ylab("Group ID") +
         ggplot2::ggtitle(title) +
         ggplot2::theme(axis.text.x = ggplot2::element_blank(),
-                      axis.ticks = ggplot2::element_blank()) +
+                       axis.ticks = ggplot2::element_blank()) +
         ggplot2::geom_point() + ggplot2::facet_wrap(~term,scales="free_x", ncol = 1) +
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
-                                   xmax=condval +2*condsd), height=0) +
+                                             xmax=condval +2*condsd), height=0) +
         ggplot2::coord_flip()
-
+      
     }else if(grey == FALSE){
-
+      
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Red", "Green", "Blue"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
         ggplot2::ylab("Group ID") +
         ggplot2::ggtitle(title) +
         ggplot2::theme(axis.text.x = ggplot2::element_blank(),
-                      axis.ticks = ggplot2::element_blank()) +
+                       axis.ticks = ggplot2::element_blank()) +
         ggplot2::geom_point() + ggplot2::facet_wrap(~term,scales="free_x", ncol = 1) +
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
-                                   xmax=condval +2*condsd), height=0) +
+                                             xmax=condval +2*condsd), height=0) +
         ggplot2::coord_flip()
-
+      
     }
-
+    
   }
-
+  print(caterpillar.plot)
+  
 }
