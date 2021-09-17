5e41ce87a939a52c7c2459f77c79f4030c4a78c1
Christof Fehrman <lj5yn@virginia.edu>
2021-09-17 15:31:52 -0400

    Adding lme4:: to lmer in examples.

--- R/rsqmlm.R
+++ R/rsqmlm.R
@@ -15,7 +15,7 @@
 #' \donttest{
 #' data(instruction)
 #' center(dataset = instruction, x = "mathkind", grouping = "classid")
-#' mod <- lmer(mathgain ~ classid_mathkind.cmn + classid_mathkind.devcmn + (1 | classid), data = instruction)
+#' mod <- lme4::lmer(mathgain ~ classid_mathkind.cmn + classid_mathkind.devcmn + (1 | classid), data = instruction)
 #' rsqmlm(mod)
 #' }
 #'
