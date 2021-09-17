15ee77c8026402e1bd808943c55bbeb82c93ad76
Christof Fehrman <lj5yn@virginia.edu>
2021-09-17 15:30:12 -0400

    Adding lme4:: to lmer function in examples.

--- R/caterpillarPlot.R
+++ R/caterpillarPlot.R
@@ -20,44 +20,44 @@
 #'
 #' \donttest{
 #' data(instruction)
-#' mod <- lmer(mathgain ~ (1 | classid), data = instruction)
+#' mod <- lme4::lmer(mathgain ~ (1 | classid), data = instruction)
 #' classid_caterpillarPlot <- caterpillarPlot(mod, title = "title", grpvar = "classid", grey = TRUE)
 #' }
 #'
-#' @export caterpillarPlot 
+#' @export caterpillarPlot
 
 caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, grey = FALSE){
   condsd <- condval <- grp <- interval_95 <- NULL
-  
+
   model.re <- data.frame(lme4::ranef(model))
   model.re <- model.re[which(model.re$grpvar == grpvar),]
-  
+
   model.re$int_pos2 <- model.re$condval + 2*model.re$condsd
   model.re$int_neg2 <- model.re$condval - 2*model.re$condsd
   model.re$interval_95 <- rep(NA, length(model.re$int_pos2))
-  
+
   for(i in 1:length(model.re$int_pos2)){
-    
+
     if(model.re$int_pos2[i] > 0 & model.re$int_neg2[i] > 0){
-      
+
       model.re$interval_95[i] <- "all positive"
-      
+
     }else if(model.re$int_pos2[i] < 0 & model.re$int_neg2[i] < 0){
-      
+
       model.re$interval_95[i] <- "all negative"
-      
+
     }else{
-      
+
       model.re$interval_95[i] <- "includes 0"
-      
+
     }
-    
+
   }
-  
+
   caterpillar.plot <- if(tall == TRUE){
-    
+
     if(grey == TRUE){
-      
+
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Grey27", "Black", "Grey87"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
@@ -69,9 +69,9 @@ caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, g
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
                                              xmax=condval +2*condsd), height=0)
-      
+
     }else if(grey == FALSE){
-      
+
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Red", "Green", "Blue"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
@@ -83,13 +83,13 @@ caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, g
         ggplot2::geom_vline(ggplot2::aes(xintercept = 0), linetype = 2) +
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
                                              xmax=condval +2*condsd), height=0)
-      
+
     }
-    
+
   }else if(tall == FALSE){
-    
+
     if(grey == TRUE){
-      
+
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Grey27", "Black", "Grey87"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
@@ -102,9 +102,9 @@ caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, g
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
                                              xmax=condval +2*condsd), height=0) +
         ggplot2::coord_flip()
-      
+
     }else if(grey == FALSE){
-      
+
       ggplot2::ggplot(model.re, ggplot2::aes(y=grp,x=condval, color=interval_95)) +
         ggplot2::scale_colour_manual(values = c("Red", "Green", "Blue"), name = "95% Prediction Interval") +
         ggplot2::xlab("Empirical Bayes Prediction") +
@@ -117,10 +117,10 @@ caterpillarPlot <- function(model, grpvar, title = print(grpvar), tall = TRUE, g
         ggplot2::geom_errorbarh(ggplot2::aes(xmin=condval -2*condsd,
                                              xmax=condval +2*condsd), height=0) +
         ggplot2::coord_flip()
-      
+
     }
-    
+
   }
   print(caterpillar.plot)
-  
+
 }
