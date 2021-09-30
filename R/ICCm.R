1087e47c5ad53a92993ad64fcf834e0fdbc39378
Christof Fehrman <lj5yn@virginia.edu>
2021-09-30 08:32:10 -0400

    Changed class to ICCm. Changed print to return. Removed print function.

--- R/ICCm.R
+++ R/ICCm.R
@@ -127,7 +127,7 @@ ICCm <- function(model, re_type = c("NA")) {
       	"ICC1" = round(icc1,3),
       	"ICC2" = round(icc2,3),
       	"ICC3" = round(icc3,3)))
-   		class(res) <- "mlmtools_icc"
+   		class(res) <- "ICCm"
    		return(res)
 	# two random effects - re_type = 'cc' - cross-classified model
  	} else if(lme4::getME(model, name = 'n_rtrms') == 2 & re_type == 'cc') {
@@ -141,29 +141,10 @@ ICCm <- function(model, re_type = c("NA")) {
       	"ICC1" = round(icc1,3),
       	"ICC2" = round(icc2,3),
       	"ICC3" = round(icc3,3)))
-   		class(res) <- "mlmtools_icc"
-   		print.mlmtools_icc(res)
+   		class(res) <- "ICCm"
+   		return(res)
  	}
 
 }
 
-#' @method print mlmtools_icc
-#' @export
-
-print.mlmtools_icc <- function(x){
-  # one random effect
-  if(x$RandEff == 1){
-    cat("Proportion of variance at the", x$factor1, "level:", x$ICC)
-    # two random effects - re_type = 'nested' - three level model
-  } else if(x$RandEff == 2 & x$type == 'nested') {
-    cat("Proportion of variance at the", x$factor1, "level:", x$ICC1, '\n')
-    cat("Proportion of variance at the", x$factor1, "level and the", x$factor2, "level:", x$ICC2, '\n')
-    cat("Proportion of", x$factor2, "level variance at the", x$factor1, "level:", x$ICC3)
-    # two random effects - re_type = 'cc' - cross-classified model
-  } else if(x$RandEff == 2 & x$type == 'cc') {
-    cat("Likeness of", x$outcome, "values of units in the same", x$factor2, "factor but different", x$factor1, "factor:", x$ICC1, '\n')
-    cat("Likeness of", x$outcome, "values of units in the same", x$factor1, "factor but different", x$factor2, "factor:", x$ICC2, '\n')
-    cat("Likeness of", x$outcome, "values of units in the same", x$factor1, "factor and same", x$factor2, "factor:", x$ICC3)
-  }
-}
 
