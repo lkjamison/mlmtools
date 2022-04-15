6b4a25ec5e34c78f979f5cec34edb4619ac5e0d7
Laura Jamison <60159688+lj5yn@users.noreply.github.com>
2022-04-15 11:24:31 -0400

    Adding functions
    Adding estimationsPlot, levelComparePlot, and mlm_assumptions

---
+++ R/mlm_assumptions.R
@@ -0,0 +1,60 @@
+#' Reports the output of testing all assumptions for a multilevel model
+#'
+#' Reports the results from testing all assumptions of a multilevel model and provides suggestions if an assumption is not passed
+#'
+#' @param model A linear mixed-effects model of class lmerMod or lmerModLmerTest
+#'
+#' @return
+#'
+#' @details
+#'
+#' @references
+
+mlm_assumptions <- function(model) {
+
+  # Model class must be 'lmerMod' or 'lmerModLmerTest'
+  classCheck <- class(model) == "lmerMod" || class(model) == "lmerModLmerTest"
+  if (!all(classCheck)) {
+    stop("Model class is not 'lmerMod' or 'lmerModLmerTest'.", call. = FALSE)
+    return(NULL)
+  }
+
+  # Data
+
+  call <- stats::getCall(model)
+  data <- call[["data"]]
+  data <- eval(data, parent.frame())
+
+  # Original y variable
+
+  form <- deparse(call[["formula"]])
+  y <- trimws(strsplit(form, "[~+]")[[1]][1])
+
+  # Linearity
+
+  linearity.plot <- plot(resid(model),#extract the residuals
+                         data[,y]) #specify original y variable
+
+  # Homogeneity of Variance
+
+  data$model.Res<- abs(residuals(model))^2 # squares the absolute values of the residuals to provide the more robust estimate
+  Levene.model <- lm(model.Res ~ classid, data=data) #ANOVA of the squared residuals
+  homo.test <- anova(Levene.model) #displays the results
+  homo.test$`Pr(>F)`[1] >= .05
+  Plot.model <- plot(model) #creates a fitted vs residual plot
+
+  # Normally distributed residuals
+
+  qqnorm(resid(model))
+  qqline(resid(model))
+
+  lattice::qqmath(model, id=0.05)
+
+
+  # Multicollinearity
+
+  # Autocorrelation
+
+
+
+}
