9e6f7c3600a0776cc992b6fc6f6835bdd62dfaab
Laura Jamison <60159688+lj5yn@users.noreply.github.com>
2022-04-15 12:37:11 -0400

    Adding to do's and resources
    Added in questions for what to further update/future directions for sections and added in notes on resources for further research/citations

--- R/mlm_assumptions.R
+++ R/mlm_assumptions.R
@@ -11,6 +11,12 @@
 #' @references
 
 mlm_assumptions <- function(model) {
+  # Resources:
+
+  #https://stats.stackexchange.com/questions/77891/checking-assumptions-lmer-lme-mixed-models-in-r
+  #https://stats.stackexchange.com/questions/376273/assumptions-for-lmer-models
+  #https://ademos.people.uic.edu/Chapter18.html#62_assumption_2_homogeneity_of_variance
+  #https://bookdown.org/animestina/phd_july_19/testing-the-assumptions.html
 
   # Model class must be 'lmerMod' or 'lmerModLmerTest'
   classCheck <- class(model) == "lmerMod" || class(model) == "lmerModLmerTest"
@@ -32,6 +38,8 @@ mlm_assumptions <- function(model) {
 
   # Linearity
 
+  ##### TO DO: should these plot all variables?
+
   linearity.plot <- plot(resid(model),#extract the residuals
                          data[,y]) #specify original y variable
 
@@ -50,11 +58,13 @@ mlm_assumptions <- function(model) {
 
   lattice::qqmath(model, id=0.05)
 
-
   # Multicollinearity
+
+  ##### TO DO: vif is from car, eigen requires numeric
 
   # Autocorrelation
 
-
+  ##### TO DO: do we need this? Longitudinal argument?
 
 }
+
