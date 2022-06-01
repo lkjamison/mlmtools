77f868622b73ce1978da16f78e660825eed9cc81
Laura Jamison <60159688+lj5yn@users.noreply.github.com>
2022-06-01 12:27:27 -0400

    Adding invisible
    print() instead of cat prduces a list output with [1] at the start and prints \n, adding invisible to see if that works

--- R/prints.R
+++ R/prints.R
@@ -104,8 +104,10 @@ print.varCompare <- function(x, ...){
 print.mlm_assumptions <- function(x, ...){
   if(x$homo.test$`Pr(>F)`[1] >= .05){
     print("Homogeneity of variance assumption met.\n")
+    invisible("Homogeneity of variance assumption met.\n")
   } else {
     print("Homogeneity of variance assumption NOT met. See: TO DO ADD RESOURCES\n")
+    invisible("Homogeneity of variance assumption NOT met. See: TO DO ADD RESOURCES\n")
   }
   if(is.character(x$multicollinearity)){
     cat(x$multicollinearity)
