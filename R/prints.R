85704ef5935edbe528d0624ec6e528a2210ecc02
Laura Jamison <60159688+lj5yn@users.noreply.github.com>
2022-06-01 12:56:41 -0400

    Adding {}

--- R/prints.R
+++ R/prints.R
@@ -102,7 +102,7 @@ print.varCompare <- function(x, ...){
 # Print mlm_assumptions
 #' @export
 print.mlm_assumptions <- function(x, message = TRUE, ...){
-  if(message)
+  if(message){
     message(if(x$homo.test$`Pr(>F)`[1] >= .05){
     cat("Homogeneity of variance assumption met.\n")
     invisible("Homogeneity of variance assumption met.\n")
@@ -125,4 +125,5 @@ print.mlm_assumptions <- function(x, message = TRUE, ...){
     cat("No outliers detected.\n")
   }
   cat("Visually inspect all plot objects.  See ?mlm_asssumptions for more information on how to inspect these plots."))
+  }
 }
